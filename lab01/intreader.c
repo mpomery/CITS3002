4d8913a4155e8949a638a753a7a711627ed79cd7
mpomery <bob_george33@hotmail.com>
2014-04-15 18:10:01 +0800

    Works for all ints
    Between -50 and 49 Inclusive

--- lab01/intreader.c
+++ lab01/intreader.c
@@ -3,29 +3,32 @@
  *		gcc -Wall -std=c99 -pedantic -o intreader intreader.c
  */
 
+
+// Info on SPARC
+// http://docs.oracle.com/cd/E19205-01/819-5265/bjbdt/index.html
+
+
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
 
 int main(int argc, char **argv)
 {
 	FILE *file;
-	file = fopen("Q4.output", "r");
+	file = fopen("Q4.output", "rb");
 	
 	printf("size of int:  %d\n", sizeof(int));
 	printf("size of char: %d\n", sizeof(char));
 	
-	for (int i = 0; i < 100; i++) {
+	for (int i = -50; i < 50; i++) {
 		int number = 0;
+		
 		char c0, c1, c2, c3;
+		
 		fread(&c0, sizeof(char), 1, file); // byte0 of sparc
 		fread(&c1, sizeof(char), 1, file);
 		fread(&c2, sizeof(char), 1, file);
 		fread(&c3, sizeof(char), 1, file); // byte3 of sparc
-		// push chars into int
 		number = ((((((number | c0) << 8) | c1) << 8) | c2) << 8) | c3;
-		printf("%i\t", number);
+		printf("%i\n", number);
 	}
 	
 	fclose(file);
